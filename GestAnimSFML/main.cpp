136fdabd07bcf45bf30ed0c0fb0ccdff564cd000
Vadimador <vadgolda@gmail.com>
2020-09-30 21:14:32 +0200

    l'argent payant c'est mieux !

--- GestAnimSFML/main.cpp
+++ GestAnimSFML/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 void main() {
-	std::cout << "De l'argent GRATUIT !\n";
+	std::cout << "Nath : De l'argent GRATUIT !\n";
+	std::cout << "Vadim : oui mec !\n";
 	system("pause>nul");
 }
