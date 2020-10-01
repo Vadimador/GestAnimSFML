02abb9f308f87b6915a2aa8f1dcffa902a978cf8
Vadimador <vadgolda@gmail.com>
2020-10-01 22:51:22 +0200

    Tentative de liaison avec SFML
    j'avoue c'était galère mdr

--- GestAnimSFML/main.cpp
+++ GestAnimSFML/main.cpp
@@ -1,8 +1,26 @@
 #include <iostream>
+#include "SFML/Graphics.hpp"
 
-void main() {
-	std::cout << "De l'argent PAYANT !\n";
-	std::cout << "Nath : De l'argent GRATUIT !\n";
-	std::cout << "Vadim : oui mec !\n";
-	system("pause>nul");
+int main()
+{
+    std::cout << "";
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
 }
