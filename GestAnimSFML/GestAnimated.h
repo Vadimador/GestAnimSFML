0f7b603037cc58ba2a42b26a80ddc2d90c2e443c
Vadimador <vadgolda@gmail.com>
2020-10-22 20:16:11 +0200

    Structure principale terminé

--- GestAnimSFML/GestAnimated.h
+++ GestAnimSFML/GestAnimated.h
@@ -6,5 +6,12 @@ class GestAnimated
 public:
 	virtual sf::Vector2f getGestSize() const = 0;
 	virtual sf::Vector2f getGestPosition() const = 0;
+	virtual sf::Vector2f getGestRotationAxis() const = 0;
+	virtual sf::Vector2f getGestRotation() const = 0;
+
+	virtual sf::Vector2f setGestSize() const = 0;
+	virtual sf::Vector2f setGestPosition() const = 0;
+	virtual sf::Vector2f setGestRotationAxis() const = 0;
+	virtual sf::Vector2f setGestRotation() const = 0;
 };
 
